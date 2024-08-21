b62e3e3f5b20ddb40a931258cd68549637cb26bd
DionisCeb <162382338+DionisCeb@users.noreply.github.com>
2024-08-21 13:20:54 -0400

    Delete public directory

--- public/js/scrolling.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('#reserver-sec-btn').addEventListener('click', function(event) {
-    event.preventDefault();
-    const targetSection = document.getElementById('reserve-sec');
-    targetSection.scrollIntoView({ behavior: 'smooth' });
-});
