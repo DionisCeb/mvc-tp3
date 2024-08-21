5c33bd178c7526b4bc82af84313a49dc890efefa
DionisCeb <162382338+DionisCeb@users.noreply.github.com>
2024-08-21 13:21:12 -0400

    Delete views directory

--- views/layouts/footer.php
+++
@@ -1,6 +0,0 @@
-    </main>
-    <footer>
-        <p>Copy Right 2024</p>
-    <footer>
-</body>
-</html>
--- views/layouts/nav.php
+++
@@ -1,9 +0,0 @@
-<div class="page-links">
-    <a class="nav-btn" href="">À propos</a>
-    <a class="nav-btn" href="">Catalogue</a>
-    <a class="nav-btn" href="">Équipe</a>
-    <a class="nav-btn" href="">Blog</a>
-</div>
-<div class="contact-us">
-    <a class="contact-btn" href="">Contactez-nous</a>
-</div>
